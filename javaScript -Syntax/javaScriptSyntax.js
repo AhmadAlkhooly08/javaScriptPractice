ad6609fe960a8a135506f1e07f9bf3cded87747b
AhmadAlkhooly08 <ahmadalkhooly08@gmail.com>
2020-06-13 15:16:49 +0300

    this is my best code

--- javaScript -Syntax/javaScriptSyntax.js
+++ javaScript -Syntax/javaScriptSyntax.js
@@ -1,3 +1,19 @@
+//hi i am ahmad
 let ahmad = 'ahmad'
 let age =12
 let grade = "seventh grade!" 
+alert('HELLO');
+undefined
+confirm('do not posh cancel');
+false
+confirm('do not posh cancel');
+true
+prompt('what are you doing with javaScript');
+"good"
+console.log('hello world');
+const name ='ahmad'; 
+const age = 12;
+const rating = 4.5;
+const isCool = true;
+const x = null;
+const y = undefined;
