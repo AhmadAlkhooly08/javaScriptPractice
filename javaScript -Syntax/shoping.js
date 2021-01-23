cea92f846d69adfecfe07994997924b73a93ffac
AhmadAlkhooly08 <ahmadalkhooly08@gmail.com>
2021-01-24 00:00:36 +0300

    i made a new file

---
+++ javaScript -Syntax/shoping.js
@@ -0,0 +1,28 @@
+let question = prompt("what do you want to buy from my website");
+const products = [];
+while(question !== "quit" && question !== "q"){
+    if(question === "list"){
+        console.log("*******");
+        for(let i = 0; i < products.length; i++){
+            console.log(`${i}: ${products[i]}`)
+        }
+        console.log("*******");
+    }
+    else if(question == "new"){
+        const item = prompt(' buy a new something');
+        products.push(item)
+        alert(`${item} added to the list`);
+    }
+    else if(question === 'delete'){
+        const deleteItem = prompt("ok, what do you want to delete");
+        for(let i = 0; i<products.length; i++){
+            if(products[i]===deleteItem){
+                alert(`you have deleted ${products[i]}`);
+                products.splice(i,1);
+            }
+            
+        }
+    }
+    question = prompt("what do you want to buy from my website");
+}
+alert('you quit the app');
