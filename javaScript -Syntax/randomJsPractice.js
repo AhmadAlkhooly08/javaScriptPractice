a5c8182a59c67c50e97b010f377d230c68890c18
AhmadAlkhooly08 <ahmadalkhooly08@gmail.com>
2021-03-01 23:12:11 +0300

    i am still in js

--- javaScript -Syntax/randomJsPractice.js
+++ javaScript -Syntax/randomJsPractice.js
@@ -1,4 +1,4 @@
-console.log("welcome back to JS Ahmad!!!!!!");
+// console.log("welcome back to JS Ahmad!!!!!!");
 // let year = 2000;
 // let age = 2;
 // age = age +10;
@@ -6,8 +6,31 @@ console.log("welcome back to JS Ahmad!!!!!!");
 // let age = 123;
 // age = false;
 // age = 345;
-let name = 'ahmad';
-name[3]
+// let name = 'ahmad';
+// name[3]
+// name.length
+// name.trim()
+// name.toUpperCase()
+// name.indexOf('h')
+// console.log(name.slice(2)) 
+// name.length
+// console.log(name.replace('a','gf'))
+// console.log(alert(name))
+// console.log(prompt(name))
+let rate = prompt('put a rate');
+if(rate <= 1 && rate <= 2){
+    console.log('baaaad!!!!!')
+}
+else if(rate === 3 || rate === 4 ){
+    console.log('not bad')
+}
+else if(rate === 5){
+    console.log('gooood')
+}
 
-
-
+else{
+    console.log('gooooood!!!!!!')
+}
+1000>100 && 10<20
+10<9 || 100 > 10
+!10>9 
