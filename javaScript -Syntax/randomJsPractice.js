78edf42d33263dfdad4ed069c5b3dab4195973ed
AhmadAlkhooly08 <ahmadalkhooly08@gmail.com>
2021-03-11 00:23:40 +0300

    function

--- javaScript -Syntax/randomJsPractice.js
+++ javaScript -Syntax/randomJsPractice.js
@@ -1,51 +1,51 @@
-// console.log("welcome back to JS Ahmad!!!!!!");
+console.log("welcome back to JS Ahmad!!!!!!");
 let year = 2008;
-// age = age +10;
-// year = year +8;
+age = age +10;
+year = year +8;
 let age = 13;
-// age = false;
-// age = 345;
+age = false;
+age = 345;
 let name = 'ahmad';
-// name[3]
-// name.length
-// name.trim()
-// name.toUpperCase()
-// name.indexOf('h')
-// console.log(name.slice(2)) 
-// name.length
-// console.log(name.replace('a','gf'))
-// console.log(alert(name))
-// let all =[name,year,age];
-// console.log(all.push('rate','mhf')) 
-// console.log(all.pop()) 
-// all.unshift("dbafebg")
-// all.shift()
-// let ad =['dwg','edf','reds'];
-// ad[1]='dweg';
-// ad.indexOf('edf')
-//ad.reverse()
-//ad.slice(1)
-//ad.includes('dwg')
-//ad.splice(1, 0, 'Feb');
-//ad.sort();
-// let a1 = ['a','b','c']
-// let a2 = ['d','e','f','g']
-// let a3 = a1.concat(a2);
+name[3]
+name.length
+name.trim()
+name.toUpperCase()
+name.indexOf('h')
+console.log(name.slice(2)) 
+name.length
+console.log(name.replace('a','gf'))
+console.log(alert(name))
+let all =[name,year,age];
+console.log(all.push('rate','mhf')) 
+console.log(all.pop()) 
+all.unshift("dbafebg")
+all.shift()
+let ad =['dwg','edf','reds'];
+ad[1]='dweg';
+ad.indexOf('edf')
+ad.reverse()
+ad.slice(1)
+ad.includes('dwg')
+ad.splice(1, 0, 'Feb');
+ad.sort();
+let a1 = ['a','b','c']
+let a2 = ['d','e','f','g']
+let a3 = a1.concat(a2);
 
-// let rate = prompt('put a rate');
-// if(rate <= 1 && rate <= 2){
-//     alert('baaaad!!!!!')
-// }
-// else if(rate === 3 || rate === 4 ){
-//     alert('not bad')
-// }
-// else if(rate === 5){
-//     alert('gooood')
-// }
+let rate = prompt('put a rate');
+if(rate <= 1 && rate <= 2){
+    alert('baaaad!!!!!')
+}
+else if(rate === 3 || rate === 4 ){
+    alert('not bad')
+}
+else if(rate === 5){
+    alert('gooood')
+}
 
-// 1000>100 && 10<20
-// 10<9 || 100 > 10
-// !10>9 
+1000>100 && 10<20
+10<9 || 100 > 10
+!10>9 
 let person = {
     name:'AHMAD',
     lastName:'ALKHOOLY',
@@ -54,16 +54,16 @@ let person = {
 
 };
 document.getElementById("demo").innerHTML = person.name  +  person.lastName + " is " + person.age + " years old. " + 'lives in ' + person.home ;
-// person.name
-// person['age']
-// person.age = 13
-// person.home = 'istanbul-turkey'
-// for(let i=1; i<=10; i++){
-//     console.log(i);
-// }
-// for(let i=0; i< person.length; i++){
-//     console.log(i,person[i]);
-// }
+person.name
+person['age']
+person.age = 13
+person.home = 'istanbul-turkey'
+for(let i=1; i<=10; i++){
+    console.log(i);
+}
+for(let i=0; i< person.length; i++){
+    console.log(i,person[i]);
+}
 for(let i=0; i<=40; i+=2){
     console.log(i);
 }
@@ -90,13 +90,22 @@ for(let a of as){
     console.log(a)
 }
 
-for(let per in  person){
-    console.log(`${per}:${person[per]}`);
-}
+ for(let per in  person){
+     console.log(`${per}:${person[per]}`);
+ }
 
-function ahmet(){
-    console.log('my name is ahmet');
-    console.log('i am in turkey');
-    console.log('i am 13 years old')
-}
-ahmet();
+ function ahmet(){
+     console.log('my name is ahmet');
+     console.log('i am in turkey');
+     console.log('i am 13 years old')
+ }
+ ahmet();
+ function ahh(){
+     console.log('jygrsxkmy rsxkmyj');
+ }
+ ahh()
+ ahh()
+ ahh()
+ ahh()
+ ahh()
+ ahh()
