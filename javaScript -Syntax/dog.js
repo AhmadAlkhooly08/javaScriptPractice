59e928e598bc6201d60798b4a31ead2d1eab31ef
AhmadAlkhooly08 <ahmadalkhooly08@gmail.com>
2021-04-16 01:15:52 +0300

    still in Dom

--- javaScript -Syntax/dog.js
+++ javaScript -Syntax/dog.js
@@ -2,6 +2,8 @@ document.all[79].innerText='about this website'
 document.getElementById('lh')
 document.getElementById('p')
 let all= document.getElementsByTagName('p');
+console.log(all, "hjgiuyficbij;")
 for(let peragraph of all){
+    console.log(all)
     console.log(peragraph.innerText);
 }
