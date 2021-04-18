88d51c0426bc593b4057ea3642702ea72a5a9de9
AhmadAlkhooly08 <ahmadalkhooly08@gmail.com>
2021-04-18 12:06:42 +0300

    now in querySelector

--- javaScript -Syntax/dog.js
+++ javaScript -Syntax/dog.js
@@ -1,4 +1,4 @@
-document.all[79].innerText='about this website'
+// document.all[79].innerText='about this website'
 document.getElementById('lh')
 document.getElementById('p')
 let all= document.getElementsByTagName('p');
@@ -7,3 +7,13 @@ for(let peragraph of all){
     console.log(all)
     console.log(peragraph.innerText);
 }
+const elements = document.getElementsByTagName('p','img','a');
+console.log(elements)
+console.log( all[0])
+console.log(document.getElementsByTagName('div'))
+let className = document.getElementsByClassName('container2');
+console.log(className);
+console.log(document.querySelector('p'));
+console.log(document.querySelector('#p'));
+console.log(document.querySelector('.card'));
+console.log(document.querySelector('p:nth-of-type(2)'));
