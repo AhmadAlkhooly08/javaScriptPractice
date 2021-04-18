d9901f9fe0f9b745718a43c29a6e520452b2e63f
AhmadAlkhooly08 <ahmadalkhooly08@gmail.com>
2021-04-19 00:20:30 +0300

    puery

--- javaScript -Syntax/dog.js
+++ javaScript -Syntax/dog.js
@@ -11,9 +11,17 @@ const elements = document.getElementsByTagName('p','img','a');
 console.log(elements)
 console.log( all[0])
 console.log(document.getElementsByTagName('div'))
-let className = document.getElementsByClassName('container2');
+let className = document.getElementsByClassName('card');
 console.log(className);
 console.log(document.querySelector('p'));
 console.log(document.querySelector('#p'));
 console.log(document.querySelector('.card'));
 console.log(document.querySelector('p:nth-of-type(2)'));
+let query = document.querySelectorAll('a');
+console.log(document.querySelectorAll('div a'))
+console.log(document.querySelectorAll('p i'))
+for(let all of query){
+    console.log(query,'kug')
+    console.log(all.href)
+    console.log(all)
+}
