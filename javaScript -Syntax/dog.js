f83672ce1541ae5cb0b468eeb301c132f3dc20e0
AhmadAlkhooly08 <ahmadalkhooly08@gmail.com>
2021-04-20 01:04:37 +0300

    puery

--- javaScript -Syntax/dog.js
+++ javaScript -Syntax/dog.js
@@ -1,4 +1,3 @@
-// document.all[79].innerText='about this website'
 document.getElementById('lh')
 document.getElementById('p')
 let all= document.getElementsByTagName('p');
@@ -17,7 +16,10 @@ console.log(document.querySelector('p'));
 console.log(document.querySelector('#p'));
 console.log(document.querySelector('.card'));
 console.log(document.querySelector('p:nth-of-type(2)'));
-let query = document.querySelectorAll('a');
+let query = document.querySelectorAll('img');
+for(let select of query){
+    // console.log(select.src='https://www.udemy.com/staticx/udemy/images/v6/logo-coral-light.svg')
+}
 console.log(document.querySelectorAll('div a'))
 console.log(document.querySelectorAll('p i'))
 for(let all of query){
@@ -25,3 +27,7 @@ for(let all of query){
     console.log(all.href)
     console.log(all)
 }
+let text = document.querySelector('#p h3').innerText='about this website'
+console.log(document.querySelector('#p p').textContent)
+console.log(document.querySelector('#p p').innerHTML)
+console.log(document.querySelector('#p h3').innerHTML='<h4>about this website</h4>')
