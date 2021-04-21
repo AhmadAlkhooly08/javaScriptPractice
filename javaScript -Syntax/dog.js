c8d81b59b45f74a276ef0eccc3410bd2ced9adbd
AhmadAlkhooly08 <ahmadalkhooly08@gmail.com>
2021-04-22 00:03:42 +0300

    style

--- javaScript -Syntax/dog.js
+++ javaScript -Syntax/dog.js
@@ -31,13 +31,22 @@ let text = document.querySelector('#peragraph h3').innerText='about this website
 console.log(document.querySelector('#peragraph p').textContent)
 console.log(document.querySelector('#peragraph p').innerHTML)
 console.log(document.querySelector('#peragraph h3').innerHTML='<h4>about this website</h4>')
-//console.log(document.querySelector('h1').innerHTML+='<h4>about this website</h4>')
+// console.log(document.querySelector('h1').innerHTML+='<h4>about this website</h4>')
 console.log(document.querySelector('#peragraph').id='#peragraph')
 console.log(document.querySelector('a').href)
 console.log(document.querySelector('img').src)
 let attrebute=document.querySelector('img');
 console.log(attrebute.getAttribute('src'))
-//console.log(attrebute.setAttribute('src','https://www.udemy.com/staticx/udemy/images/v6/logo-coral-light.svg'))
+// console.log(attrebute.setAttribute('src','https://www.udemy.com/staticx/udemy/images/v6/logo-coral-light.svg'))
 let inputt = document.querySelector('input');
 inputt.type='password'
 inputt.type='color'
+let h1= document.querySelector('h1');
+h1.style
+console.log(h1.style.color)
+// h1.style.fontSize = '20px'
+// h1.style.border = '2px solid red'
+console.log(window.getComputedStyle(h1).color)
+console.log(window.getComputedStyle(h1).padding)
+console.log(window.getComputedStyle(h1).fontFamily)
+console.log(window.getComputedStyle(h1).fontSize)
